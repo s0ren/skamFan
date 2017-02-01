1253af5edb6d517b6ac79232c60be8a07105e7af
Søren Magnusson <smag@tec.dk>
2017-02-01 11:28:46 +0100

    Afsnit i sæson halvt færdig

--- www/js/index.js
+++ www/js/index.js
@@ -26,7 +26,7 @@ function prepareSeasons(event, ui)
                 // lad JQM forbedre htmlen
                 $('#seasons').enhanceWithin();
                 // tilføj event handler til hver knap
-                $('#seasons').one('click', prepareSeason);
+                $('#seasons a').one('click', prepareSeason);
             }
 
         }
@@ -34,7 +34,41 @@ function prepareSeasons(event, ui)
 }
 
 function prepareSeason(event, ui)
-{}
+{
+    console.log("Indlæser sæson " + this.dataset.slug );
+
+    $.get('http://www.dr.dk/mu-online/api/1.3/list/view/season?id='+ this.dataset.slug +'&limit=15&offset=0',
+        function(res, code){
+            console.debug(code + ": " + JSON.stringify(res));
+
+            // check lige om indholdes er bare lidt i orden...
+            if (res.Items && res.TotalSize > 0)
+            {
+                // fjern 'gamle' div'er
+                //$('#season div').remove();
+                var newContent = '';
+                // gennemløb alle afsnit i Items
+                for (var i in res.Items) {
+                    // lav en ny "div" for hver sæson
+                    // <div>
+                    //     <a href="#episodeDetails">
+                    //         <img src="xxx">
+                    //         <h2>2 3-10</h2>
+                    //     <h2>Jonas kysser Eva, men Noora ser det hele</h2>
+                    //     </a>
+                    //     </div>
+                    newContent += '<div><a href="#episodeDetails">'
+                                + '<img src="xxx">'
+                                + '<h2>'+ res.Items[i].Title +'</h2>'
+                                + '<h2>Jonas kysser Eva, men Noora ser det hele</h2>'
+                                + '</a></div>';
+                }
+
+            }
+
+        }
+    )
+}
 
 $(document).ready( // når siden er loaded
     function(){
