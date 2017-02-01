1b67c918e419e485903a0e3630431bf241da9165
Søren Magnusson <smag@tec.dk>
2017-02-01 09:51:50 +0100

    Sæsoner indsættes dynamisk fra DR-mu .

--- www/js/index.js
+++ www/js/index.js
@@ -6,10 +6,29 @@ function prepareSeasons(event, ui)
 
     $.get("http://www.dr.dk/mu-online/api/1.3/list/view/seasons?id=skam&onlyincludefirstepisode=true&limit=15&offset=0",
         function(res, code) {
-        console.log(code + ": " + JSON.stringify(res));
+            console.debug(code + ": " + JSON.stringify(res));
+
+            // check lige om indholdes er bare lidt i orden...
+            if (res.Items && res.TotalSize > 0)
+            {
+                $('#seasons a').remove();
+                var newContent = '';
+                for (var i in res.Items)
+                {
+                    // <a href="#episodes" data-role="button" data-slug="">Sæson 1</a>
+                    newContent += '<a href="#episodes" data-role="button" data-slug="'+res.Items[i].Slug+'">Sæson ' + res.Items[i].SeasonNumber + '</a>';
+                }
+                $(newContent).appendTo('#seasons');
+                $('#seasons').enhanceWithin();
+                $('#seasons').one('click', prepareSeason);
+            }
+
         }
     )
 }
+
+function prepareSeason(event, ui)
+{}
 
 $(document).ready( // når siden er loaded
     function(){
